be3b14c65d1a44aef0c12191167397c35d45d128
HJR <hjr.jan.rumanek@gmail.com>
2025-06-16 17:02:08 +0200

    Added package functionality
    It is now possible to import endecoder as a python package and use the encode() and decode() functions; These functions take one input and return the encrypted/decrypted form respectively.

--- endecoder.py
+++ endecoder.py
@@ -9,24 +9,34 @@ codes = {
     'Symbols_ENC_sel': ('ⵒ','ⵝ','ⵞ','ⵠ')
 }
 
-
-ED = input("Encrypt or decrypt? [E/D]:")[0].lower()
-input = input("Input text to " + ("encrypt" if ED == 'e' else  "decrypt") + ":").lower()
-output = ''
-
-if ED == 'e':
-    for letter in input:
+def encrypt(text):
+    output = ''
+    for letter in text:
         if letter in codes['Alphanum']:
             output += random.choice(codes['Alphanum_ENC_sel'])
             output += codes['Alphanum_ENC'][codes['Alphanum'].index(letter)]
         elif letter in codes['Symbols']:
             output += random.choice(codes['Symbols_ENC_sel'])
             output += codes['Symbols_ENC'][codes['Symbols'].index(letter)]
-else:
-    for i in range(int(len(input)/2)):
-        if input [i*2] in codes['Alphanum_ENC_sel']:
-            output += codes['Alphanum'][codes['Alphanum_ENC'].index(input[i*2+1])]
-        elif input [i*2] in codes['Symbols_ENC_sel']:
-            output += codes['Symbols'][codes['Symbols_ENC'].index(input[i*2+1])]
+    return output
 
-print(output)
+def decrypt(text):
+    output = ''
+    for i in range(int(len(text)/2)):
+        if text [i*2] in codes['Alphanum_ENC_sel']:
+            output += codes['Alphanum'][codes['Alphanum_ENC'].index(text[i*2+1])]
+        elif text [i*2] in codes['Symbols_ENC_sel']:
+            output += codes['Symbols'][codes['Symbols_ENC'].index(text[i*2+1])]
+    return output
+
+if __name__ == '__main__':
+    ED = input("Encrypt or decrypt? [E/D]:")[0].lower()
+    input = input("Input text to " + ("encrypt" if ED == 'e' else  "decrypt") + ":").lower()
+    output = ''
+
+    if ED == 'e':
+        output = encrypt(input)
+    else:
+        output = decrypt(input)
+        
+    print(output)
