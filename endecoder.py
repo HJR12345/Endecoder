afc87c80976a846a0ffd09d556e4d06fdd24671c
HJR <hjr.jan.rumanek@gmail.com>
2025-06-16 16:44:37 +0200

    Initial commit
    Added endecoder.py

---
+++ endecoder.py
@@ -0,0 +1,32 @@
+import random
+
+codes = {
+    'Alphanum': ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'),
+    'Symbols': ('!','@','#','$','%','^','&','*','(',')','<','>','?','/','\\','|','~','_','-','+','=','{','}','[',']',';',':',',','.','`',"'",'"',' '),
+    'Alphanum_ENC': ('ⴰ','ⴱ','ⴳ','ⴷ','ⴹ','ⴻ','ⴼ','ⴽ','ⵀ','ⵃ','ⵄ','ⵅ','ⵇ','ⵉ','ⵊ','ⵍ','ⵎ','ⵏ','ⵓ','ⵔ','ⵕ','ⵖ','ⵙ','ⵚ','ⵛ','ⵜ','ⵟ','ⵡ','ⵢ','ⵣ','ⵥ','ⵯ','ⴲ','ⴴ','ⴵ','ⴶ'),
+    'Symbols_ENC': ('ⴰ','ⴱ','ⴳ','ⴷ','ⴹ','ⴻ','ⴼ','ⵀ','ⵃ','ⵄ','ⵅ','ⵇ','ⵊ','ⵍ','ⵎ','ⵏ','ⵓ','ⵔ','ⵕ','ⵖ','ⵙ','ⵚ','ⵛ','ⵜ','ⵟ','ⵡ','ⵢ','ⵣ','ⵥ','ⵯ','ⴲ','ⴴ','ⴵ'),
+    'Alphanum_ENC_sel': ('ⴸ','ⴺ','ⴿ','ⵁ'),
+    'Symbols_ENC_sel': ('ⵒ','ⵝ','ⵞ','ⵠ')
+}
+
+
+ED = input("Encrypt or decrypt? [E/D]:")[0].lower()
+input = input("Input text to " + ("encrypt" if ED == 'e' else  "decrypt") + ":").lower()
+output = ''
+
+if ED == 'e':
+    for letter in input:
+        if letter in codes['Alphanum']:
+            output += random.choice(codes['Alphanum_ENC_sel'])
+            output += codes['Alphanum_ENC'][codes['Alphanum'].index(letter)]
+        elif letter in codes['Symbols']:
+            output += random.choice(codes['Symbols_ENC_sel'])
+            output += codes['Symbols_ENC'][codes['Symbols'].index(letter)]
+else:
+    for i in range(int(len(input)/2)):
+        if input [i*2] in codes['Alphanum_ENC_sel']:
+            output += codes['Alphanum'][codes['Alphanum_ENC'].index(input[i*2+1])]
+        elif input [i*2] in codes['Symbols_ENC_sel']:
+            output += codes['Symbols'][codes['Symbols_ENC'].index(input[i*2+1])]
+
+print(output)
